d6fa53f4350de114a55decbca4925facd792e56a
Saqib <zaman342003@gmail.com>
2022-02-20 22:40:43 +0500

    Updates

---
+++ precache-manifest.2a55cd9e4cedc68c2fb88fd162bf4aa3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7e619d405b9c86e43bc0ffaf823a4ad7",
+    "url": "/monsters-rolodexes/index.html"
+  },
+  {
+    "revision": "5cce6aebe9f1f223524b",
+    "url": "/monsters-rolodexes/static/css/main.a142fb8a.chunk.css"
+  },
+  {
+    "revision": "efe1924630397efd073d",
+    "url": "/monsters-rolodexes/static/js/2.b4309f00.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monsters-rolodexes/static/js/2.b4309f00.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5cce6aebe9f1f223524b",
+    "url": "/monsters-rolodexes/static/js/main.b03ba4b4.chunk.js"
+  },
+  {
+    "revision": "d74dbd960eb2e0a59891",
+    "url": "/monsters-rolodexes/static/js/runtime-main.eee56fc1.js"
+  }
+]);
